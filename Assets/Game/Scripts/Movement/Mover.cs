f0ffa8bd00a2d5cbae60bcb27d0de38d3598b979
Marat <mar.davidoff@gmail.com>
2024-03-19 12:03:24 +0300

    Add minimap + object rotation

--- Assets/Game/Scripts/Movement/Mover.cs
+++ Assets/Game/Scripts/Movement/Mover.cs
@@ -1,43 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 using RPG.Core;
 using RPG.Saving;
 
 namespace RPG.Movement
 {
-    
-    //scripts uses:
-    //actionscheduler.cs
     public class Mover : MonoBehaviour, IAction, ISaveable
     {
-        //[SerializeField] Transform target;
+        public ParticleSystem effectPrefab; // Ссылка на объект Particle System
 
-        //cached
         private Animator animator;
         private NavMeshAgent thisNavAgent;
-        //private Ray lastRay;
         private ActionScheduler actionScheduler;
 
-        // Start is called before the first frame update
         void Start()
         {
-            if(!thisNavAgent)
+            if (!thisNavAgent)
                 thisNavAgent = GetComponent<NavMeshAgent>();
-            if(!actionScheduler)
+            if (!actionScheduler)
                 actionScheduler = GetComponent<ActionScheduler>();
-            
+
             animator = GetComponent<Animator>();
-            
         }
 
-        // Update is called once per frame
         void Update()
         {
             UpdateAnimator();
 
-            //Debug.DrawRay(lastRay.origin, lastRay.direction * 100)
+            // Проверяем, было ли нажатие мыши
+            if (Input.GetMouseButtonDown(0))
+            {
+                CreateEffectAtMousePosition();
+            }
         }
 
         public void StartMoveAction(Vector3 pos)
@@ -45,68 +39,61 @@ namespace RPG.Movement
             actionScheduler.StartAction(this);
             MoveTo(pos);
         }
-        
+
         public void MoveTo(Vector3 pos)
         {
             thisNavAgent.destination = pos;
             thisNavAgent.isStopped = false;
         }
-        
+
         private void UpdateAnimator()
         {
-            //converts velocity  of nav mesh agent from world space to local space
             if (!thisNavAgent) return;
-                
-            Vector3 localVelocity = transform.InverseTransformDirection(thisNavAgent.velocity);
 
-            ///update animator value
+            Vector3 localVelocity = transform.InverseTransformDirection(thisNavAgent.velocity);
             animator.SetFloat("forwardSpeed", localVelocity.z);
         }
 
         public void Cancel()
         {
             thisNavAgent.isStopped = true;
-            
         }
 
         public bool IsAtLocation(float tolerance)
         {
-            
-            return Vector3.Distance(thisNavAgent.destination , transform.position) < tolerance;
+            return Vector3.Distance(thisNavAgent.destination, transform.position) < tolerance;
         }
 
-        
-        //isaveable override
         public object CaptureState()
         {
-            print("saving location state for " + gameObject);
-            
-            //if(gameObject == MainPlayer.Instance.gameObject)
-                //print(transform.position);
-            
             return new SerializableVector3(transform.position);
         }
 
-        
         public void RestoreState(object state)
         {
-            print("loading previous state for " + gameObject);
-            
-            //ex: state corresponds to the value that is associated with a unique identifier key
-            SerializableVector3 oldPos = (SerializableVector3) state;
-            
-            //NOTE: due to function timing, start will be called after wrapper.load (From portal) so necessary to
-            //retrieve references to preent null pointer
+            SerializableVector3 oldPos = (SerializableVector3)state;
             actionScheduler = GetComponent<ActionScheduler>();
             thisNavAgent = GetComponent<NavMeshAgent>();
-            
-            //need to disable the navAgent before adjusting the position
             thisNavAgent.enabled = false;
-            
             transform.position = oldPos.ToVector3();
-            //Warning: this throws warning if player is not near navmesh...
             thisNavAgent.enabled = true;
-            
+        }
+
+        // Метод для создания эффекта на поверхности в указанной позиции
+        private void CreateEffectAtMousePosition()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                CreateEffect(hit.point + new Vector3(0, 0.1f, 0));
+            }
+        }
+
+        // Метод для создания эффекта в указанной позиции
+        private void CreateEffect(Vector3 position)
+        {
+            Instantiate(effectPrefab, position, Quaternion.identity);
         }
     }
 }
